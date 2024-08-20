0341c3b7877d5114fe109ee24e6cb754ab446566
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2024-08-20 01:00:00 -0300

    Header e footer; conta (html/css) não finalizado

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function navigateTo(destination) {
         console.log("Não foi possível viajar até esta página")
     }
 }
+
+function writeIn(elementId, text) {
+    document.getElementById(elementId).innerHTML = text;
+}
+
+(function alerta(alertClass) {
+    let elements = document.getElementsByClassName(alertClass)
+    
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        element.innerHTML = "Conteúdo Incompleto"
+    }
+})("alerta-incompleto")
