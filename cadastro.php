ab45a12e5718709ca695b9f30cc06facd09160f3
miguel_borges <miguelborgess2016@gmail.com>
2024-10-24 14:39:46 -0300

    .

---
+++ cadastro.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/cadastro.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="" method="post">
+
+    <section>
+        <label for="nome">Nome: </label>
+        <input type="text" name = "nome" required>
+    </section>
+
+    <section>
+        <label for="email">Digite seu Email: </label>
+        <input type="email" name = "email" required>
+    </section>
+
+    <section>
+        <label for="senha">Digite sua senha: </label>
+        <input type="password" name = "senha" required>
+    </section>
+
+    <section>
+        <label for="comf_senha">Comfirme a sua senha: </label>
+        <input type="password" name = "comf_senha" required>
+    </section>
+
+    <input type="submit" >
+        
+ </form>
+    
+</body>
+</html>
