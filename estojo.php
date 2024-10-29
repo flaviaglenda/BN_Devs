5f976c4a31f17827709b646b35374c9f3e154208
lannakamilly <lannakamillyfresmota@gmail.com>
2024-10-29 06:29:19 -0300

    lanna comit

---
+++ estojo.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/pagprod.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Catamaran:wght@200&family=Courgette&family=Dancing+Script:wght@700&family=Edu+TAS+Beginner:wght@700&family=Lato:wght@300;900&family=Mukta:wght@700&family=Mulish:wght@300&family=Open+Sans&family=PT+Sans:ital,wght@1,700&family=Poppins:wght@300&family=Raleway:wght@100&family=Roboto&family=Roboto+Condensed:wght@700&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f30fac2c61.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+      
+
+       
+
+        <div class="shop" id="shop">
+            <div class="head">
+                <h1>Cadernos</h1>
+            </div>
+            <div class="cards">
+                <div class="crd">
+                    <img src="./img/es1.jpeg" alt="">
+              
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <h3>R$10</h3>
+                </div>
+                <div class="crd">
+                    <img src="./img/es2.jpeg" alt="">
+                
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <h3>R$20</h3>
+                </div>
+                <div class="crd">
+                    <img src="./img/es3.jpeg" alt="">
+           
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <h3>R$30</h3>
+                </div>
+            </div>
+        </div>
+
+        
+
+       
+</body>
+</html>
